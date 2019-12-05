9e4f90428d5d6d50b2d2c1093c14d5311aff397e
xuchangjian <xuchangjian>
2019-12-05 18:18:11 +0800

    Initial commit

--- src/plugins/mongodb.ts
+++ src/plugins/mongodb.ts
@@ -2,7 +2,7 @@
  * @Description: mongodb
  * @Author: 徐长剑
  * @Date: 2019-11-27 17:47:24
- * @LastEditTime: 2019-11-28 14:12:31
+ * @LastEditTime: 2019-12-05 18:15:46
  * @LastEditors: 徐长剑
  */
 // import { MongooseModule } from '@nestjs/mongoose';
@@ -14,7 +14,7 @@ const { username, host, port, database, password } = Config.Mongodb;
 // const DB_URL = `mongodb://${username}:${password}@${host}:${port}/${database}`;
 
 // export default MongooseModule.forRoot(DB_URL);
-
+console.log(username, host, port, database, password, '====');
 export default TypeOrmModule.forRoot({
   type: 'mongodb',
   host,
